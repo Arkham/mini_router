2c352fda1f0b78b5bac0560aeece2983161a3005
Ju Liu <ju.liu@welaika.com>
2017-01-28 01:05:24 +0000

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,42 @@
+defmodule MiniRouter.Mixfile do
+  use Mix.Project
+
+  @target System.get_env("NERVES_TARGET") || "rpi3"
+
+  def project do
+    [app: :mini_router,
+     version: "0.1.0",
+     target: @target,
+     archives: [nerves_bootstrap: "~> 0.2.1"],
+
+     deps_path: "deps/#{@target}",
+     build_path: "_build/#{@target}",
+
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     aliases: aliases(),
+     deps: deps() ++ system(@target)]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [mod: {MiniRouter, []},
+     applications: [:logger]]
+  end
+
+  def deps do
+    [{:nerves, "~> 0.4.0"}]
+  end
+
+  def system(target) do
+    [{:"nerves_system_#{target}", ">= 0.0.0"}]
+  end
+
+  def aliases do
+    ["deps.precompile": ["nerves.precompile", "deps.precompile"],
+     "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
+  end
+
+end
---
+++ test/mini_router_test.exs
@@ -0,0 +1,8 @@
+defmodule MiniRouterTest do
+  use ExUnit.Case
+  doctest MiniRouter
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
